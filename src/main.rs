cf38af3c72eda14b85bf9c473894bb10991f3c72
Pawel Latawiec <pawel@etiket.dev>
2022-10-15 22:54:42 -0400

    ADD spawn obstacles

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,7 @@ mod app {
             clocks::Clocks,
             rtc::{Rtc, RtcInterrupt},
             twim, Timer,
+            rng,
         },
         pac::{
             self,
@@ -27,12 +28,28 @@ mod app {
         },
     };
 
+    use cortex_m::interrupt::Mutex;
+    use core::{cell::RefCell};
+
+    use rand::{RngCore, SeedableRng};
+    use rand_pcg::Pcg32;
+
+    static RNG: Mutex<RefCell<Option<Pcg32>>> = Mutex::new(RefCell::new(None));
+
+
     use lsm303agr::{interface::I2cInterface, mode, AccelMode, AccelOutputDataRate, Lsm303agr};
 
     pub struct Game {
         player: Player,
+        obstacle: Obstacle,
         track_speed: u8,
         score: u32,
+        rng: rand_pcg::Lcg64Xsh32
+    }
+
+    struct Obstacle {
+        board_position_x: usize,
+        position_y: i32,
     }
 
     struct Player {
@@ -56,9 +73,12 @@ mod app {
     const TRACK_MIN: i32 = -2000;
     const TRACK_MAX: i32 = 2000;
     const TRACK_MARGIN: i32 = 500; // track area for "gutters"
+    const BASE_VELOCITY: u8 = 100;
 
-    fn render_state(player: &Player) -> GreyscaleImage {
+    fn render_state(game: &Game) -> GreyscaleImage {
         // returns a GrayscaleImage from the game state
+        let player = &game.player;
+        let obstacle = &game.obstacle;
         let mut data = [
             [0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0],
@@ -67,16 +87,18 @@ mod app {
             [0, 0, 0, 0, 0],
         ];
 
-        let board_position = player_board_position(player);
+        let board_position = get_board_position(player.position);
+        let board_position_obstacle_y = get_board_position(obstacle.position_y);
 
         data[4][board_position as usize] = 9;
+        data[board_position_obstacle_y][obstacle.board_position_x] = 5;
         GreyscaleImage::new(&data)
     }
 
-    fn player_board_position(player: &Player) -> u8 {
+    fn get_board_position(position: i32) -> usize {
         let track_delta = (TRACK_MAX - TRACK_MIN + 2 * TRACK_MARGIN) / 5;
-        let pos = player.position - TRACK_MIN + TRACK_MARGIN;
-        (pos / track_delta).clamp(0, 4) as u8
+        let pos = position - TRACK_MIN + TRACK_MARGIN;
+        (pos / track_delta).clamp(0, 4) as usize
     }
 
     #[init]
@@ -106,17 +128,28 @@ mod app {
 
         sensor.set_accel_mode(AccelMode::Normal).unwrap();
 
+        /* Use hardware RNG to initialise PRNG */
+        let mut rng = rng::Rng::new(board.RNG);
+        let mut seed: [u8; 16] = [0; 16];
+        let rng = Pcg32::from_seed(seed);
+        
         let display = Display::new(board.TIMER1, board.display_pins);
 
         let mut player = Player {
-            position: 0,
+            position: TRACK_MIN,
             velocity: 0,
             acceleration: 0,
         };
+        let mut obstacle = Obstacle {
+            board_position_x: 2,
+            position_y: TRACK_MIN,
+        };
         let mut game = Game {
             player,
+            obstacle,
             track_speed: 1,
             score: 0,
+            rng,
         };
         (
             Shared { display },
@@ -141,9 +174,19 @@ mod app {
         let mut shared = cx.shared;
         let local = cx.local;
         let mut player = &mut local.game.player;
+        let mut obstacle = &mut local.game.obstacle;
 
         local.game_clock.reset_event(RtcInterrupt::Tick);
 
+        // check collisions
+
+        // "spawn" new obstacle
+        if obstacle.position_y > TRACK_MAX {
+            let position_x = local.game.rng.next_u32() / (u32::MAX / 5);
+            obstacle.board_position_x = position_x.clamp(0, 4) as usize;
+            obstacle.position_y = TRACK_MIN;
+        }
+        
         let data = local.sensor.accel_data().unwrap();
 
         // 1 game tick = 1/60 sec
@@ -157,11 +200,18 @@ mod app {
         player.velocity = player.velocity.clamp(-100, 100);
         player.position += player.velocity;
         player.position = player.position.clamp(TRACK_MIN, TRACK_MAX);
+
+        obstacle.position_y += (BASE_VELOCITY as i32) * (local.game.track_speed as i32);
         
         shared.display.lock(|display| {
-            display.show(&render_state(&player));
+            display.show(&render_state(&local.game));
         });
 
         local.game.score += 1;
+
+        // Speed up every minute
+        if local.game.score % 3600 == 0 {
+            local.game.track_speed += 1;
+        }
     }
 }
